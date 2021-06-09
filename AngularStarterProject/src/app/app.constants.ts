5bf342a9a155c2fde1090cb201abf15220d8113e
GavriloS <gavrilo.stanic2@gmail.com>
2021-06-09 08:56:10 +0200

    Test 2

--- AngularStarterProject/src/app/app.constants.ts
+++ AngularStarterProject/src/app/app.constants.ts
@@ -1,9 +1,20 @@
-export const FAKULTET_URL = 'http://localhost:8084/fakultet';
+// export const FAKULTET_URL = 'http://localhost:8084/fakultet';
 
-export const DEPARTMAN_URL = 'http://localhost:8084/departman'
+// export const DEPARTMAN_URL = 'http://localhost:8084/departman'
 
-export const STATUS_URL = 'http://localhost:8084/status'
+// export const STATUS_URL = 'http://localhost:8084/status'
 
-export const STUDENT_URL = 'http://localhost:8084/student';
+// export const STUDENT_URL = 'http://localhost:8084/student';
 
-export const STUDENT_IN_DEPARTMENT = 'http://localhost:8084/studentiSaDepartmana';
+// export const STUDENT_IN_DEPARTMENT = 'http://localhost:8084/studentiSaDepartmana';
+
+
+export const FAKULTET_URL = 'https://backend-rva2021-it31-2018.herokuapp.com/fakultet';
+
+export const DEPARTMAN_URL = 'https://backend-rva2021-it31-2018.herokuapp.com/departman'
+
+export const STATUS_URL = 'https://backend-rva2021-it31-2018.herokuapp.com/status'
+
+export const STUDENT_URL = 'https://backend-rva2021-it31-2018.herokuapp.com/student';
+
+export const STUDENT_IN_DEPARTMENT = 'https://backend-rva2021-it31-2018.herokuapp.com/studentiSaDepartmana';
